f4dcc180f66de7264e17433bb42e370b1462c64e
Rohit Singh <kala.r447y@gmail.com>
2023-12-21 17:49:44 +0530

    provider file here

---
+++ terraform-s3/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  backend "s3" {      #here state file send in s3 bucket
+   bucket = "terraform-backup23"  #Bucket name
+   key    = "Terraform/terraform.tfstate"  #File Name
+   region = "us-east-2"
+ }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+  #profile = "default"
+  access_key = "my_access_key"  
+  secret_key = "my_secret_key"
+}
+
